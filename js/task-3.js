390893a9b267cc0e867c697e7f434b1f2ebc9ed1
kalops1a <vadymduchenko@gmail.com>
2024-01-31 19:18:13 +0100

    5

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
 function getElementWidth(content, padding, border) {
-    const number1 = Number.parseFloat(content);
-    const number2 = Number.parseFloat(padding);
-    const number3 = Number.parseFloat(border);
-    return number1 + number2 + number3;
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding);
+    const borderWidth = Number.parseFloat(border);
+    return contentWidth + paddingWidth + borderWidth;
 }
 
-onsole.log(getElementWidth("50px", "8px", "4px")); // 74
+сonsole.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
